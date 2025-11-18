60f1651fad2fd200a2708697f8ee8af139474aeb
vmbansal2001 <vmbansal2001@gmail.com>
2025-11-18 21:39:11 +0530

    feat: configure pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import withPWA from "next-pwa";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development",
+}) as (config: NextConfig) => NextConfig;
+
+export default pwaConfig(nextConfig);
