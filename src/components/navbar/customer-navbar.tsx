37826a6728e75d8413547954628d5c642423d448
vmbansal2001 <vmbansal2001@gmail.com>
2025-11-24 00:34:04 +0530

    feat: add logout functionality to CustomerNavbar
    - Implemented handleLogout function to sign out users
    - Added logout button with icon for user convenience
    - Enhanced user greeting section with improved layout

--- src/components/navbar/customer-navbar.tsx
+++ src/components/navbar/customer-navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import LogoWithName from "../logos/logo-with-name";
 import useUserData from "../use-user-data/use-user-data";
+import { LogOut } from "lucide-react";
+import { auth } from "@/src/firebase.config";
 
 const CustomerNavbar = () => {
   const { userData } = useUserData();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+  };
 
   return (
     <nav className="bg-white shadow-sm">
@@ -21,8 +27,17 @@ const CustomerNavbar = () => {
           </Link>
         </div>
 
-        <div className="text-sm text-gray-600 font-medium">
-          Hello, {userData.full_name}
+        <div className="flex items-center gap-2">
+          <div className="text-sm text-gray-600 font-medium">
+            Hello, {userData.full_name}
+          </div>
+
+          <button
+            onClick={handleLogout}
+            className="text-red-600 hover:text-red-600 cursor-pointer transition-all duration-200"
+          >
+            <LogOut className="w-4 h-4" />
+          </button>
         </div>
       </div>
     </nav>
