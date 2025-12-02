6b9985e39bd098ca5c0fe9daa8384384c2e82b27
vmbansal2001 <vmbansal2001@gmail.com>
2025-12-03 01:24:14 +0530

    feat: improve notification permission handling with user association
    - Integrated user data retrieval to associate FCM tokens with user IDs.
    - Updated permission request logic to only execute when a user ID is available.
    - Enhanced API call to save the FCM token along with the user ID to the backend.

--- src/components/notification-permission-handler.tsx
+++ src/components/notification-permission-handler.tsx
@@ -2,17 +2,35 @@
 
 import { requestNotificationPermission } from "@/src/firebase.config";
 import { useEffect } from "react";
+import useUserData from "./use-user-data/use-user-data";
 
 export default function NotificationPermissionHandler() {
+  const { userData } = useUserData();
+
+  const userId = userData?.id;
+
   const askPermission = async () => {
     const token = await requestNotificationPermission();
-    console.log("FCM TOKEN:", token);
-    // Save token to Firestore / your backend
+
+    if (token && userId) {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/v1/user_devices/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ user_id: userId, fcm_token: token }),
+        }
+      );
+    }
   };
 
   useEffect(() => {
-    askPermission();
-  }, []);
+    if (userId) {
+      askPermission();
+    }
+  }, [userId]);
 
   return null;
 }
